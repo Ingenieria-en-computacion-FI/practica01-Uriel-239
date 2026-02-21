adf1b15b48e01f90d86d50d33569b7b65cf6c29c
Uriel Salgado <uri.auss@gmail.com>
2026-02-21 10:37:20 -0600

    Enhance main function with array memory details
    Added comments explaining array bounds and memory access in C.

--- soluciones/ej9.c
+++ soluciones/ej9.c
@@ -1,6 +1,21 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 9
+
+int main() {
+    int a[5];
+
+    printf("&a[-1] = %p\n", &a[-1]);
+    printf("&a[5]  = %p\n", &a[5]);
+
     return 0;
 }
+
+/* * 
+1. ¿POR QUÉ COMPILA?: C no verifica los límites del arreglo en tiempo de compilación. Solo traduce 
+ el índice a una dirección de memoria matemática y ejecuta la instrucción.
+ 
+2. ¿POR QUÉ ES PELIGROSO?: Accedes a memoria que no esta reservada. Esto puede causar error de otros datos, 
+ comportamientos raros o errores de segmentación.
+ 
+3. ¿QUÉ REVELA SOBRE LA MEMORIA?: Confirma que la memoria es lineal y contigua. Revela que C no guarda el 
+ tamaño del arreglo internamente; el control depende totalmente del programador.
+ */
