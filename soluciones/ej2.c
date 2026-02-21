b1f82b923e769844348f5ed0100116a861bb1130
Uriel Salgado <uri.auss@gmail.com>
2026-02-21 00:27:40 -0600

    Update ej2.c

--- soluciones/ej2.c
+++ soluciones/ej2.c
@@ -1,6 +1,10 @@
 
 #include <stdio.h>
 int main(){
-    // Resolver ejercicio 2
+    /*  I. Comparación de direcciones: La dirección mayor es &a[5] porque se ubica adelante en memoria, usando la fórmula: 
+    direccion = base + (i * tamaño(en este caso int)) 
+    &a[4] = base + (4 * 4) = base + 16    &a[5] = base + (5 * 4) = base + 20
+ Al ser la base constante, &a[5] es mayor por una diferencia de 4 bytes.
+ */
     return 0;
 }
