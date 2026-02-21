a286e11293467e73e139bad2bed2ff238332b1db
Uriel Salgado <uri.auss@gmail.com>
2026-02-21 10:32:19 -0600

    Update ej8.c

--- soluciones/ej8.c
+++ soluciones/ej8.c
@@ -1,6 +1,17 @@
 
 #include <stdio.h>
 int main(){
-    // Resolver ejercicio 8
+    /* base = 2000, tamño(int) = 4, dirección de m[1][2] = 2024
+
+ usamos la fórmula: Direccion = base + (i * N + j) * tamaño(tipo)
+ 2024 = 2000 + (1 * N + 2) * 4 
+ 24 = (N + 2) * 4
+ 6 = N + 2
+ N = 4 columnas
+
+Con 2 filas (M=2), con el índice 1 existe:
+Tamaño = M * N * tamaño(int) 
+Tamaño = 2 * 4 * 4 = 32 bytes
+ */
     return 0;
 }
